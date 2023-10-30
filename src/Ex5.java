135850179e901eca91fc0c36dadae3d2e6c11e80
Rafaell <rafaell997@outlook.com>
2023-10-30 11:49:01 -0300

    Java

---
+++ src/Ex5.java
@@ -0,0 +1,10 @@
+public class Ex5 {
+    public static void main(String[] args) {
+        double V, R, A;
+        R = 3.2;
+        A = 4.9;
+
+        V = 3.14159 * R * R * A;
+        System.out.println("O volume da lata de óleo é de " + V + " m³");
+    }
+}
